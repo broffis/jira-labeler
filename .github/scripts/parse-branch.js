4300be5f1b92195ebbc705613d899c3db412462f
Brandon Roffis <brandon.roffis@ferguson.com>
2023-10-11 16:38:47 -0400

    feat(parse): started parse

---
+++ .github/scripts/parse-branch.js
@@ -0,0 +1,13 @@
+module.exports = ({ core }, { commit, jiraPrefix }) => {
+  console.log({ commit, jiraPrefix });
+  const regex = new RegExp(`^${jiraPrefix}-\\d+`, "i");
+  const match = commit.match(regex);
+
+  if (!match) {
+    throw new Error("Commit does not match Jira ticket pattern");
+  }
+
+  const [issueNumber] = commit.match(regex);
+  core.setOutput("issueNumber", issueNumber);
+  console.log({ issueNumber });
+};
