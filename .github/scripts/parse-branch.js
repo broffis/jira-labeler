1af254dfe51ed13423c221501988d4b37167eb47
Brandon Roffis <brandon.roffis@ferguson.com>
2023-10-11 16:54:06 -0400

    EFDC-7049 - ci: removed github

--- .github/scripts/parse-branch.js
+++ .github/scripts/parse-branch.js
@@ -1,5 +1,4 @@
-module.exports = ({ core, github }, { commit, jiraPrefix }) => {
-  const githubCommit = github.event.commits[0].message;
+module.exports = ({ core }, { commit, jiraPrefix }) => {
   console.log({ commit, jiraPrefix, githubCommit });
   const regex = new RegExp(`^${jiraPrefix}-\\d+`, "i");
   const match = commit.match(regex);
