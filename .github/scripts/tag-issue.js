4cbf5d5707c7e131a107859ca0feff358cca160a
Brandon Roffis <brandon.roffis@ferguson.com>
2023-10-12 12:24:22 -0400

    EFDC-7049 - ci: test await

--- .github/scripts/tag-issue.js
+++ .github/scripts/tag-issue.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 // const auth = `${process.env.JIRA_USER}:${process.env.JIRA_KEY}`;
 
-module.exports = ({ fetch }, { issueKey, label, jiraInstance }) => {
+module.exports = async ({ fetch }, { issueKey, label, jiraInstance }) => {
   const body = {
     update: {
       labels: [{ add: label }],
@@ -19,23 +19,33 @@ module.exports = ({ fetch }, { issueKey, label, jiraInstance }) => {
 
   // const authBuffer = Buffer.from(auth).toString("base64");
 
-  fetch(`https://${jiraInstance}.atlassian.net/rest/api/2/issue/${issueKey}`, {
-    method: "PUT",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: `Basic ${Buffer.from(
-        `${process.env.JIRA_USER}:${process.env.JIRA_KEY}`
-      ).toString("base64")}`,
-      // Authorization: `Basic ${authBuffer}`,
-      // Authorization: `Basic ${auth}`,
-    },
-    body: JSON.stringify(body),
-  })
-    .then((response) => {
-      console.log(`Response: ${response.status} ${response.statusText}`);
-      return response.text();
-    })
-    .then((text) => console.log(text))
-    .catch((err) => console.error(err));
+  const response = await fetch(
+    `https://${jiraInstance}.atlassian.net/rest/api/2/issue/${issueKey}`,
+    {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Basic ${Buffer.from(
+          `${process.env.JIRA_USER}:${process.env.JIRA_KEY}`
+        ).toString("base64")}`,
+        // Authorization: `Basic ${authBuffer}`,
+        // Authorization: `Basic ${auth}`,
+      },
+      body: JSON.stringify(body),
+    }
+  );
+
+  if (response.status !== 204) {
+    throw new Error(
+      `Received unexpected status code ${response.status} ${response.statusText}`
+    );
+  }
+  console.log(`Response: ${response.status} ${response.statusText}`);
+  // .then((response) => {
+  //   console.log(`Response: ${response.status} ${response.statusText}`);
+  //   return response.text();
+  // })
+  // .then((text) => console.log(text))
+  // .catch((err) => console.error(err));
 };
