48ad18cb11e9a049d6f35abbe67422b026c01f64
Brandon Roffis <brandon.roffis@ferguson.com>
2023-10-12 11:20:50 -0400

    EFDC-7049 - ci: test auth

--- .github/scripts/tag-issue.js
+++ .github/scripts/tag-issue.js
@@ -1,10 +1,7 @@
-// const fetch = require("node-fetch");
-// import fetch from "node-fetch";
-
 const dotenv = require("dotenv");
 dotenv.config();
 
-const auth = `${process.env.JIRA_USER}:${process.env.JIRA_KEY}`;
+// const auth = `${process.env.JIRA_USER}:${process.env.JIRA_KEY}`;
 
 module.exports = ({ fetch }, { issueKey, label, jiraInstance }) => {
   const body = {
@@ -13,16 +10,24 @@ module.exports = ({ fetch }, { issueKey, label, jiraInstance }) => {
     },
   };
 
-  console.log({ auth });
+  // console.log({ auth });
+  console.log({
+    issueKey,
+    label,
+    jiraInstance,
+  });
 
-  const authBuffer = Buffer.from(auth).toString("base64");
+  // const authBuffer = Buffer.from(auth).toString("base64");
 
   fetch(`https://${jiraInstance}.atlassian.net/rest/api/2/issue/${issueKey}`, {
     method: "PUT",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
-      Authorization: `Basic ${authBuffer}`,
+      Authorization: `Basic ${Buffer.from(
+        `${process.env.JIRA_USER}:${process.env.JIRA_KEY}`
+      ).toString("base64")}`,
+      // Authorization: `Basic ${authBuffer}`,
       // Authorization: `Basic ${auth}`,
     },
     body: JSON.stringify(body),
