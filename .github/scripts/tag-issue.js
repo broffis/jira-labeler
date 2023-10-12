a996e1cec6827baed0b7f22cf88a27a9cf049726
Brandon Roffis <brandon.roffis@ferguson.com>
2023-10-12 11:09:53 -0400

    EFDC-7049 - ci: see what

--- .github/scripts/tag-issue.js
+++ .github/scripts/tag-issue.js
@@ -1,29 +1,31 @@
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
 // import fetch from "node-fetch";
 
-module.exports = (_, { issueKey, label, jiraInstance, auth }) => {
-  const body = {
-    update: {
-      labels: [{ add: label }],
-    },
-  };
+module.exports = (stuff, { issueKey, label, jiraInstance, auth }) => {
+  // const body = {
+  //   update: {
+  //     labels: [{ add: label }],
+  //   },
+  // };
 
-  const authBuffer = Buffer.from(auth).toString("base64");
+  console.log(stuff);
 
-  fetch(`https://${jiraInstance}.atlassian.net/rest/api/2/issue/${issueKey}`, {
-    method: "PUT",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: `Basic ${authBuffer}`,
-      // Authorization: `Basic ${auth}`,
-    },
-    body: JSON.stringify(body),
-  })
-    .then((response) => {
-      console.log(`Response: ${response.status} ${response.statusText}`);
-      return response.text();
-    })
-    .then((text) => console.log(text))
-    .catch((err) => console.error(err));
+  // const authBuffer = Buffer.from(auth).toString("base64");
+
+  // fetch(`https://${jiraInstance}.atlassian.net/rest/api/2/issue/${issueKey}`, {
+  //   method: "PUT",
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //     Authorization: `Basic ${authBuffer}`,
+  //     // Authorization: `Basic ${auth}`,
+  //   },
+  //   body: JSON.stringify(body),
+  // })
+  //   .then((response) => {
+  //     console.log(`Response: ${response.status} ${response.statusText}`);
+  //     return response.text();
+  //   })
+  //   .then((text) => console.log(text))
+  //   .catch((err) => console.error(err));
 };
